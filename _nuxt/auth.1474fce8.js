87da4c1264b62105dc013f974e214cf72d783a5f
Евгений Глебов <egoji@mail.ru>
2023-07-27 13:38:50 +0300

    37ab574

---
+++ _nuxt/auth.1474fce8.js
@@ -0,0 +1,1 @@
+import{D as o,x as t}from"./entry.8767be93.js";import{u as a}from"./cookie.9a7241d9.js";const n=o(()=>{if(!a("userInformation").value)return t("/authorization")});export{n as default};
---
+++ _nuxt/user.ee3dadf8.js
@@ -0,0 +1,1 @@
+import{k as t}from"./entry.8767be93.js";const i=t({id:"user-store",state:()=>({email:null,uid:null}),actions:{setUser(e,s){this.email=e,this.uid=s}}});export{i as u};
